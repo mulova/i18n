14d667190122ebc1b9d838cb283db12a300bb797
yonghun.lim <yonghun.lim@linecorp.com>
2019-10-22 01:45:48 +0900

    add SpreadSheet

---
+++ Runtime/reader/SpreadSheetColumnAttribute.cs
@@ -0,0 +1,27 @@
+﻿
+namespace mulova.i18n
+{
+	public class SpreadSheetColumnAttribute : System.Attribute {
+		public readonly string column;
+		public readonly int columnIndex;
+		public readonly object defaultValue;
+
+		public SpreadSheetColumnAttribute(string column) : this(column, null) { }
+
+		public SpreadSheetColumnAttribute(string column, object defaultValue) {
+			this.column = column;
+			this.columnIndex = GetColumnIndex(column);
+			this.defaultValue = defaultValue;
+		}
+
+		private int GetColumnIndex(string col) {
+			string uppercase = col.ToUpper();
+			int index = uppercase[0]-'A'+1;
+			for (int i=1; i<uppercase.Length; i++) {
+				index*=26;
+                index += (uppercase[i]-'A')+1;
+			}
+			return index-1;
+		}
+	}
+}
---
+++ Runtime/reader/SpreadSheetSourceType.cs
@@ -0,0 +1,16 @@
+﻿//----------------------------------------------
+// Unity3D common libraries and editor tools
+// License: The MIT License ( http://opensource.org/licenses/MIT )
+// Copyright © 2013- mulova@gmail.com
+//----------------------------------------------
+
+using System;
+
+namespace mulova.i18n
+{
+	public enum SpreadSheetSourceType
+	{
+		XML, CSV
+	}
+}
+
